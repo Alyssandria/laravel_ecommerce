74573a2224d8aa9274efac35b35ccfb0d37004e4
Alyssandria <sanchez.roniel.work@gmail.com>
2025-07-08 00:04:09 +0800

    feat: backend google oauth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+})->name('app.home');
+
+Route::get('/auth/redirect/{provider}', function ($provider) {
+    return Socialite::driver($provider)->redirect();
 });
+Route::get('/auth/callback/{provider}', [UserController::class, 'handleOauthCallback']);
